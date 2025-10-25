eea04363e88170b4fba69053c873dd8ab647e029
vincent brettnacher <vincent.brettnacher+git@gmail.com>
2025-10-25 11:39:18 +0000

    work on mongo db

--- src/ajbot/_internal/ajmongodb.py
+++ src/ajbot/_internal/ajmongodb.py
@@ -10,15 +10,10 @@ from pydantic import BaseModel, Field
 
 from ajbot._internal.config import AjConfig
 
-aj_config = AjConfig(break_if_missing=True,
-                     save_on_exit=False,                                 #TODO: change to True
-                    )
-
-
-client = AsyncMongoClient(aj_config._config_dict.get("mongodb_uri"),
-                          uuidRepresentation="standard")
-event_db = client.eventlist
-events = event_db.events
+# client = AsyncMongoClient(aj_config._config_dict.get("mongodb_uri"),
+#                         uuidRepresentation="standard")
+# event_db = client.eventlist
+# events = event_db.events
 
 class EventItem(BaseModel):
     """ class handling events
@@ -26,23 +21,45 @@ class EventItem(BaseModel):
     id: UUID = Field(default_factory=uuid4, alias="_id")
     name: str
 
-async def create_event(name):
-    """ event to create a event entry
+
+class AjDb():
+    """ get the ajbot database
     """
-    new_event = EventItem(name=name)
-    await events.insert_one(new_event.model_dump(by_alias=True))
-    return new_event
+    _aj_config: AjConfig
+    _client: AsyncMongoClient
+    _event_db : any
+    _events : any
+
+    def __init__(self, aj_config:AjConfig):
+        self._aj_config = aj_config
+        self._client = AsyncMongoClient(aj_config._config_dict.get("mongodb_uri"),
+                                        uuidRepresentation="standard")
+        self._event_db = self._client.eventlist
+        self._events = self._event_db.events
+
+    async def create_event(self, name):
+        """ event to create a event entry
+        """
+        new_event = EventItem(name=name)
+        await self._events.insert_one(new_event.model_dump(by_alias=True))
+        return new_event
 
 
 
 
-def _main():
+async def _main():
     """ main function
     """
-    name = "test event"
-    new_event = asyncio.run(create_event(name))
+    with AjConfig(break_if_missing=True,
+                     save_on_exit=False,                                 #TODO: change to True
+                    ) as aj_config:
+
+        name = "test event"
+        aj_db = AjDb(aj_config)
+        new_event = await aj_db.create_event(name)
+
     print(f"Created event: {new_event}")
     return 0
 
 if __name__ == "__main__":
-    sys.exit(_main())
+    sys.exit(asyncio.run(_main()))
