4095a17f5cd4b0c9c69dbfdf68132ab9c28a7371
vincent brettnacher <vincent.brettnacher+git@gmail.com>
2025-10-26 16:28:54 +0100

    final test with mongDB

--- src/ajbot/_internal/ajmongodb.py
+++ src/ajbot/_internal/ajmongodb.py
@@ -1,19 +1,16 @@
 """ test deployment of a MongoDB instance
 """
 import sys
-from pathlib import Path
+# from pathlib import Path
 import asyncio
 from uuid import UUID, uuid4
 
 from pymongo import AsyncMongoClient
+# from pymongo.asynchronous import database as mongodb
 from pydantic import BaseModel, Field
 
 from ajbot._internal.config import AjConfig
 
-# client = AsyncMongoClient(aj_config._config_dict.get("mongodb_uri"),
-#                         uuidRepresentation="standard")
-# event_db = client.eventlist
-# events = event_db.events
 
 class EventItem(BaseModel):
     """ class handling events
@@ -21,6 +18,9 @@ class EventItem(BaseModel):
     id: UUID = Field(default_factory=uuid4, alias="_id")
     name: str
 
+# class Movie(BaseModel):
+#     name: str
+#     year: int
 
 class AjDb():
     """ get the ajbot database
@@ -32,10 +32,16 @@ class AjDb():
 
     def __init__(self, aj_config:AjConfig):
         self._aj_config = aj_config
-        self._client = AsyncMongoClient(aj_config._config_dict.get("mongodb_uri"),
+        # self._movies = mongodb. Database[Movie] = self._client.movie
+
+    async def __aenter__(self):
+        self._client = AsyncMongoClient(self._aj_config._config_dict.get("mongodb_uri"),
                                         uuidRepresentation="standard")
         self._event_db = self._client.eventlist
         self._events = self._event_db.events
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self._client.close()
 
     async def create_event(self, name):
         """ event to create a event entry
@@ -44,6 +50,13 @@ class AjDb():
         await self._events.insert_one(new_event.model_dump(by_alias=True))
         return new_event
 
+    # async def create_movie(self, name, year):
+    #     """ event to create a movie entry
+    #     """
+    #     new_movie = Movie(name=name, year=year)
+    #     await self._movies.insert_one(new_movie.model_dump(by_alias=True))
+    #     return new_movie
+
 
 
 
@@ -51,14 +64,18 @@ async def _main():
     """ main function
     """
     with AjConfig(break_if_missing=True,
-                     save_on_exit=False,                                 #TODO: change to True
-                    ) as aj_config:
+                  save_on_exit=False,                                 #TODO: change to True
+                 ) as aj_config:
 
         name = "test event"
         aj_db = AjDb(aj_config)
         new_event = await aj_db.create_event(name)
+        print(f"Created event: {new_event}")
+        # new_movie = await aj_db.create_movie("test movie", 2020)
+        # print(f"Created movie: {new_movie}")
 
-    print(f"Created event: {new_event}")
+        await aj_db._client.close()
+
     return 0
 
 if __name__ == "__main__":
