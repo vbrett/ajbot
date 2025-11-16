7c9d0420b7ec397f2f39a1204499e4ae3fd78c59
vincent brettnacher <vincent.brettnacher+git@gmail.com>
2025-11-16 09:54:12 +0100

    clean testdb

--- tests/testdb.py
+++ tests/testdb.py
@@ -7,56 +7,65 @@ import sqlalchemy as sa
 from ajbot._internal import ajdb
 from ajbot._internal.config import FormatTypes
 
-async def _read_db():
+async def _search_member(aj_db_session, lokkup_val):
+    query_result = await aj_db_session.search_member(lokkup_val)
+    print('')
+    print('')
+    print('-------------------')
+    for qr in query_result:
+        print(f'{qr:{FormatTypes.RESTRICTED}}')
+    print('-------------------')
+    for qr in query_result:
+        print(f'{qr:{FormatTypes.FULLSIMPLE}}')
+    print('-------------------')
+    for qr in query_result:
+        print(f'{qr:{FormatTypes.FULLCOMPLETE}}')
+        print('')
+    print('-------------------')
+    print('')
+    print('')
+
+async def _season_events(aj_db_session):
+    query = sa.select(ajdb.Events).filter(ajdb.Events.is_in_current_season)
+    async with aj_db_session.AsyncSessionMaker() as session:
+        async with session.begin():
+            query_result = await session.execute(query)
+    print('')
+    print('')
+    print('-------------------')
+    matched_events = query_result.scalars().all()
+    for e in matched_events:
+        print(e)
+    print(len(matched_events), 'évènement(s)')
+    print('-------------------')
+
+async def _principal_address(aj_db_session):
+    query = sa.select(ajdb.JCTMemberAddress).where(ajdb.Members.address_principal is not None)
+    async with aj_db_session.AsyncSessionMaker() as session:
+        async with session.begin():
+            query_result = await session.execute(query)
+    print('')
+    print('')
+    print('-------------------')
+    matched_members = query_result.scalars().all()
+    for e in matched_members:
+        print(e.address)
+    print(len(matched_members), 'membre(s) avec adresse principale')
+    print('-------------------')
+
+
+async def _main():
     """ main function - async version
     """
-    async with ajdb.AjDb() as aj_db:
+    async with ajdb.AjDb() as aj_db_session:
 
-        query_result = await aj_db.search_member('vincent')
-        print('')
-        print('')
-        print('-------------------')
-        for qr in query_result:
-            print(f'{qr:{FormatTypes.RESTRICTED}}')
-        print('-------------------')
-        for qr in query_result:
-            print(f'{qr:{FormatTypes.FULLSIMPLE}}')
-        print('-------------------')
-        for qr in query_result:
-            print(f'{qr:{FormatTypes.FULLCOMPLETE}}')
-            print('')
-        print('-------------------')
-        print('')
-        print('')
+        await _search_member(aj_db_session, 'vincent')
+        await _season_events(aj_db_session)
+        await _principal_address(aj_db_session)
 
-        query = sa.select(ajdb.Events).filter(ajdb.Events.is_in_current_season)
-        async with aj_db.AsyncSessionMaker() as session:
-            async with session.begin():
-                query_result = await session.execute(query)
-        print('')
-        print('')
-        print('-------------------')
-        matched_events = query_result.scalars().all()
-        for e in matched_events:
-            print(e)
-        print(len(matched_events), 'évènement(s)')
-        print('-------------------')
-
-        query = sa.select(ajdb.JCTMemberAddress).where(ajdb.Members.address_principal is not None)
-        async with aj_db.AsyncSessionMaker() as session:
-            async with session.begin():
-                query_result = await session.execute(query)
-        print('')
-        print('')
-        print('-------------------')
-        matched_members = query_result.scalars().all()
-        for e in matched_members:
-            print(e.address)
-        print(len(matched_members), 'membre(s) avec adresse principale')
-        print('-------------------')
 
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(asyncio.run(_read_db()))
+    sys.exit(asyncio.run(_main()))
