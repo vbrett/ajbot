3e94a5f664925b2b9bedd7216ca36ae4d118fd46
vbrett <19249705+vbrett@users.noreply.github.com>
2025-12-07 21:50:16 +0100

    prepare curent_asso_role implementation

--- src/ajbot/_internal/ajdb_tables.py
+++ src/ajbot/_internal/ajdb_tables.py
@@ -320,10 +320,13 @@ class Member(Base):
     def current_asso_role(self):
         """ return current role
         """
-        current_member_asso_roles = [ar.asso_role for ar in self.asso_roles if ar.is_current_role]
-        assert(len(current_member_asso_roles) <= 1)
-        if current_member_asso_roles:
-            return [current_member_asso_roles]
+        # check for manually assigned role
+        active_manually_assigned_roles = [ar.asso_role for ar in self.asso_roles if ar.is_active]
+        assert(len(active_manually_assigned_roles) <= 1) , f"Multiple active asso roles for member {self.id}:\r\n{', '.join(str(ar) for ar in active_manually_assigned_roles)}"
+        if active_manually_assigned_roles:
+            return active_manually_assigned_roles[0].asso_role
+
+        # all_asso_roles = AjDb.query_table_content(AssoRole)
 
         # if self.is_subscriber:
         #     return [ar for ar in ][0]
@@ -902,14 +905,14 @@ class MemberAssoRole(Base):
     comment: orm.Mapped[Optional[str]] = orm.mapped_column(sa.String(255), nullable=True)
 
     @hybrid_property
-    def is_current_role(self):
-        """ return true if item is current season
+    def is_active(self):
+        """ return true if this role is currently active
         """
         return (    datetime.datetime.now().date() >= self.start
                 and (not self.end or datetime.datetime.now().date() <= self.end))
 
-    @is_current_role.expression
-    def is_current_role(cls):      #pylint: disable=no-self-argument   #function is a class factory
+    @is_active.expression
+    def is_active(cls):      #pylint: disable=no-self-argument   #function is a class factory
         """ SQL version
         """
         return  sa.select(
@@ -917,7 +920,7 @@ class MemberAssoRole(Base):
                     sa.and_(
                         datetime.datetime.now().date() >= cls.start,
                         datetime.datetime.now().date() <= cls.end)).correlate(cls), True), else_=False,
-                    ).label("is_current_role")
+                    ).label("is_active")
                 ).scalar_subquery()
 
     def __str__(self):
