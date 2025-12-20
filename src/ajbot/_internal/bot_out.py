f4b8c16fb77aad603dd182259fbd8a6c13457140
vbrett <19249705+vbrett@users.noreply.github.com>
2025-12-20 12:51:07 +0100

    handle big view content

--- src/ajbot/_internal/bot_out.py
+++ src/ajbot/_internal/bot_out.py
@@ -14,15 +14,21 @@ from ajbot._internal.exceptions import OtherException
 
 # Generic Display functions
 # ========================================================
-async def send_response_as_text(interaction: Interaction,
-                                content:str,
-                                embed=None,
-                                ephemeral=False,
-                                chunk_size=bot_config.CONTENT_MAX_SIZE, split_on_eol=True):
-    """ Send basic command response, handling splitting it if needed based on discord limit.
-        Can also ensure that split is only perform at eol.
+
+def split_text(content:str,
+               chunk_size=bot_config.CHUNK_MAX_SIZE,
+               split_on_eol=True):
+    """ Generator that splits content into chunks based on given limit.
+        Can also ensure that split is only performed at eol.
+        Yield each chunk.
+
+        Supports content being None, in which case yields None.
     """
-    assert (chunk_size <= bot_config.CONTENT_MAX_SIZE), f"La taille demandée {chunk_size} n'est pas supportée. Max {bot_config.CONTENT_MAX_SIZE}."
+    if not content:
+        yield None
+        return
+
+    assert (chunk_size <= bot_config.CHUNK_MAX_SIZE), f"La taille demandée {chunk_size} n'est pas supportée. Max {bot_config.CHUNK_MAX_SIZE}."
 
     first_answer = True
     i = 0
@@ -37,46 +43,64 @@ async def send_response_as_text(interaction: Interaction,
         if first_answer:
             first_answer = False
         else:
-            chunk = '(...)\n' + chunk
-            embed=None  # Empty embed so it is only send with the first chunk (even though, I doubt we will ever have embed with 2000+ content)
+            chunk = bot_config.CHUNK_CONTINUE + chunk
+        yield chunk
 
+
+async def send_response_as_text(interaction: Interaction,
+                                content:str,
+                                embed=None,
+                                ephemeral=False):
+    """ Send basic command response, handling splitting it if needed based on discord limit.
+        Can also ensure that split is only perform at eol.
+    """
+
+    for chunk in split_text(content):
         if interaction.response.type:
-            await interaction.followup.send(content=chunk, embed=embed, ephemeral=ephemeral)
+            message_fct = interaction.followup.send
         else:
-            await interaction.response.send_message(content=chunk, embed=embed, ephemeral=ephemeral)
+            message_fct = interaction.response.send_message
+
+        await message_fct(content=chunk, embed=embed, ephemeral=ephemeral)
+        embed = None  # Empty embed so it is only send with the first chunk (even though, I doubt we will ever have embed with 2000+ content)
 
 
 async def send_response_as_view(interaction: Interaction,
                                 container:dui.Container=None,
                                 title:str=None, summary:str=None, content:str=None,
                                 ephemeral=False,):
-    """ Send command response as a view
+    """ Send command response as a view, splitting content if needed.
     """
-    assert container is not None or title is not None or summary is not None or content is not None
+    assert container is not None or title is not None or summary is not None or content is not None, "Il faut fournir soit un container, soit des éléments à ajouter."
+    assert not (container is not None and (title is not None or summary is not None or content is not None)), "Soit on fournit un container, soit des éléments à ajouter, mais pas les deux."
 
-    if not container:
-        view = dui.LayoutView()
-        container = dui.Container()
-        view.add_item(container)
+    for chunk in split_text(content):
+        if not container:
+            view = dui.LayoutView()
+            container = dui.Container()
+            view.add_item(container)
 
-        if title:
-            container.add_item(dui.TextDisplay(f'# __{title}__'))
-        if summary:
-            container.add_item(dui.TextDisplay(f'## {summary}'))
-        if content:
-            container.add_item(dui.TextDisplay(f'>>> {content}'))
+            if title:
+                container.add_item(dui.TextDisplay(f'# __{title}__'))
+            if summary:
+                container.add_item(dui.TextDisplay(f'## {summary}'))
+            if chunk:
+                container.add_item(dui.TextDisplay(f'>>> {chunk}'))
 
-    timestamp = discord.utils.format_dt(interaction.created_at, 'F')
-    footer = dui.TextDisplay(f'-# Généré par {interaction.user} (ID: {interaction.user.id}) | {timestamp}')
+        timestamp = discord.utils.format_dt(interaction.created_at, 'F')
+        footer = dui.TextDisplay(f'-# Généré par {interaction.user} (ID: {interaction.user.id}) | {timestamp}')
 
-    container.add_item(footer)
+        container.add_item(footer)
 
-    if interaction.response.type:
-        message_fct = interaction.followup.send
-    else:
-        message_fct = interaction.response.send_message
+        if interaction.response.type:
+            message_fct = interaction.followup.send
+        else:
+            message_fct = interaction.response.send_message
 
-    await message_fct(view=container.view, ephemeral=ephemeral)
+        await message_fct(view=container.view, ephemeral=ephemeral)
+        container = None
+        title = None        #display title & summary only on first chunk
+        summary = None
 
 
 # Member, event,.. detail display
